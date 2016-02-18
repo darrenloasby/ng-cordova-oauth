4c24ac709a5a5550668360e87fa951e09bf0a20d
Darren Loasby <darren.loasby@gmail.com>
2016-02-19 09:19:44 +1100

    Updated nber to concat dist

--- dist/ng-cordova-oauth.js
+++ dist/ng-cordova-oauth.js
@@ -1274,66 +1274,61 @@ function meetup($q, $http, $cordovaOauthUtility) {
 
 meetup.$inject = ['$q', '$http', '$cordovaOauthUtility'];
 
-angular.module('oauth.github', ['oauth.utils'])
-  .factory('$ngCordovaGithub', github);
+angular.module('oauth.nationbuilder', ['oauth.utils'])
+  .factory('$ngCordovaNationbuilder', nationbuilder);
 
-function github($q, $http, $cordovaOauthUtility) {
-  return { signin: oauthGithub };
+function nationbuilder($q, $http, $cordovaOauthUtility) {
+  return { signin: oauthNationbuilder };
 
   /*
-   * Sign into the GitHub service
+   * Sign into the Nationbuilder service
    *
-   * @param    string clientId
-   * @param    string clientSecret
-   * @param    array appScope
+   * @param    string appKey
    * @param    object options
    * @return   promise
    */
-  function oauthGithub(clientId, clientSecret, appScope, options) {
+  function oauthNationbuilder(appKey, options) {
     var deferred = $q.defer();
     if(window.cordova) {
       if($cordovaOauthUtility.isInAppBrowserInstalled()) {
-        var redirect_uri = "http://localhost/callback";
+        var redirect_uri = "http://localhost/oauth_callback";
         if(options !== undefined) {
           if(options.hasOwnProperty("redirect_uri")) {
             redirect_uri = options.redirect_uri;
           }
         }
-        var browserRef = window.cordova.InAppBrowser.open('https://github.com/login/oauth/authorize?client_id=' + clientId + '&redirect_uri=' + redirect_uri + '&scope=' + appScope.join(","), '_blank', 'location=no,clearsessioncache=yes,clearcache=yes');
-        browserRef.addEventListener('loadstart', function(event) {
-          if((event.url).indexOf(redirect_uri) === 0) {
-            requestToken = (event.url).split("code=")[1];
-            $http.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
-            $http.defaults.headers.post.accept = 'application/json';
-            $http({method: "post", url: "https://github.com/login/oauth/access_token", data: "client_id=" + clientId + "&client_secret=" + clientSecret + "&redirect_uri=" + redirect_uri + "&code=" + requestToken })
-              .success(function(data) {
-                deferred.resolve(data);
-              })
-              .error(function(data, status) {
-                deferred.reject("Problem authenticating");
-              })
-              .finally(function() {
-                setTimeout(function() {
-                    browserRef.close();
-                }, 10);
-              });
+        var browserRef = window.cordova.InAppBrowser.open("https://gu.nationbuilder.com/oauth/authorize?client_id=" + appKey + "&redirect_uri=" + redirect_uri + "&response_type=token");
+        browserRef.addEventListener("loadstart", function(event) {
+          if ((event.url).indexOf(redirect_uri) === 0) {
+            browserRef.removeEventListener("exit",function(event){});
+            browserRef.close();
+            var callbackResponse = (event.url).split("#")[1];
+            var responseParameters = (callbackResponse).split("&");
+            var parameterMap = [];
+            for(var i = 0; i < responseParameters.length; i++) {
+              parameterMap[responseParameters[i].split("=")[0]] = responseParameters[i].split("=")[1];
+            }
+            if(parameterMap.access_token !== undefined && parameterMap.access_token !== null) {
+              deferred.resolve({ access_token: parameterMap.access_token, token_type: parameterMap.token_type, uid: parameterMap.uid });
+            } else {
+              deferred.reject("Problem authenticating");
+            }
           }
         });
         browserRef.addEventListener('exit', function(event) {
-            deferred.reject("The sign in flow was canceled");
+          deferred.reject("The sign in flow was canceled");
         });
       } else {
-          deferred.reject("Could not find InAppBrowser plugin");
+        deferred.reject("Could not find InAppBrowser plugin");
       }
     } else {
       deferred.reject("Cannot authenticate via a web browser");
     }
-
     return deferred.promise;
   }
 }
 
-github.$inject = ['$q', '$http', '$cordovaOauthUtility'];
+nationbuilder.$inject = ['$q', '$http', '$cordovaOauthUtility'];
 
 angular.module('oauth.odnoklassniki', ['oauth.utils'])
   .factory('$ngCordovaOdnoklassniki', odnoklassniki);
